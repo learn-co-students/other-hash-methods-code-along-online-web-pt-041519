844b888ecf37b3b83b314b86756bfe3cae04ba2d
johncban <johncban@gmail.com>
2019-05-09 06:31:35 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -10,4 +10,14 @@ require 'pry'
 
 def get_the_min(groceries)
   #code your solution here!
+
+  #letters = {a: 1, b: 2}
+
+  #letters.min
+
+  #alphabetical_order = {c: 100, d: 1}
+  #alphabetical_order.min
+
+  groceries.values.flatten.min
+
 end
